ba88e14111b56b3bc9c34f095fe4d4612f9a14bc
BrianBahk <61204977+BrianBahk@users.noreply.github.com>
2022-03-12 16:05:45 -0600

    Update Shooter.java

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,12 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+import edu.wpi.first.math.kinematics.MecanumDriveKinematics;
+import edu.wpi.first.math.kinematics.MecanumDriveOdometry;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -10,23 +15,69 @@ public class Shooter extends SubsystemBase {
     private final TalonFX top;
     private final TalonFX bottom;
     private double motorSpeed;
+    // private final MecanumDriveOdometry odometry;
+    // private final MecanumDriveKinematics kinematics;
 
     public Shooter() {
         top = new TalonFX(Constants.SHOOTER_TOP);
         bottom = new TalonFX(Constants.SHOOTER_BOTTOM);
+
         top.configFactoryDefault();
         bottom.configFactoryDefault();
+
         bottom.setInverted(true);
-        motorSpeed = 0;
+        top.setSensorPhase(false);
+        bottom.follow(top);
+        top.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+        bottom.configSelectedFeedbackSensor(FeedbackDevice.IntegratedSensor, 0, 0);
+
+        top.configNeutralDeadband(0.001);
+        bottom.configNeutralDeadband(0.001);
+
+        top.configNominalOutputForward(0, Constants.kTimeoutMs);
+        top.configNominalOutputReverse(0, Constants.kTimeoutMs);
+        top.configPeakOutputForward(1, Constants.kTimeoutMs);
+        top.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+
+        bottom.configNominalOutputForward(0, Constants.kTimeoutMs);
+        bottom.configNominalOutputReverse(0, Constants.kTimeoutMs);
+        bottom.configPeakOutputForward(1, Constants.kTimeoutMs);
+        bottom.configPeakOutputReverse(-1, Constants.kTimeoutMs);
+        double val = 0.28;
+        top.config_kP(Constants.kPIDLoopIdx, val, Constants.kTimeoutMs);
+        top.config_kF(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
+
+        top.config_kD(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs); // start with small kp, then double until
+                                                                       // oscillations, then increase d
+        top.config_kI(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
+
+        bottom.config_kP(Constants.kPIDLoopIdx, val, Constants.kTimeoutMs);
+        bottom.config_kF(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
+
+        bottom.config_kD(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
+        top.config_kI(Constants.kPIDLoopIdx, 0, Constants.kTimeoutMs);
+
+        top.setNeutralMode(NeutralMode.Brake);
+        bottom.setNeutralMode(NeutralMode.Brake);
+
     }
 
     @Override
     public void periodic() {
-        top.set(TalonFXControlMode.PercentOutput, motorSpeed);
-        bottom.set(TalonFXControlMode.PercentOutput, motorSpeed);
+        double speed = motorSpeed * 2000.0 * 2048.0 / 600.0;
+        top.set(TalonFXControlMode.Velocity, speed);
+        bottom.set(TalonFXControlMode.Velocity, speed);
+
+        System.out.println(top.getSelectedSensorVelocity());
+        System.out.println(bottom.getSelectedSensorVelocity());
     }
 
-    public void on() {motorSpeed = 0.8;}
+    public void on() {
+        motorSpeed = 0.8;
+    }
 
-    public void off() {motorSpeed = 0;}
+    public void off() {
+        motorSpeed = 0;
+    }
+
 }
